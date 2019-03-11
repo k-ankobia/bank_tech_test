ff75e16b6c92917bf33baee34e0b12217aa02b39
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-03-11 18:13:15 +0000

    :bug: :white_check_mark: Fixed print_balance and date tests

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,11 +1,11 @@
 class Statement
   attr_reader :date
   def print_balance
-    'date || credit || debit || balance'
+    print "date || credit || debit || balance"
   end
 
   def get_date
     @date = Time.now.strftime('%Y-%m-%d')
-    @date
+    print @date
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,18 +1,15 @@
 require 'statement'
 
 describe Statement do
-  context '.printBalance' do
+  context '.print_balance' do
     it 'Can print bank statement title' do
-      expect { subject.printBalance.to output('date || credit || debit || balance') }
+      expect { subject.print_balance }.to output("date || credit || debit || balance").to_stdout
     end
   end
 
   context '#date' do
     it 'Can print the current date' do
-      # @date = Time.now.strftime('%Y-%m-%d')
-      # allow(Time).to receive(:now).and_return(@date)
-      # expect { subject.get_date }.to output(@date).to_stdout
-      expect { subject.get_date.to output("2019-03-11")}
+      expect { subject.get_date }.to output("2019-03-11").to_stdout
     end
   end
 end
