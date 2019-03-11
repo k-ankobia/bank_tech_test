4b016afbf59703fba1fc58c0498414b8efb6101c
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-03-11 15:57:11 +0000

    :sparkles: added functionality to deposit amount

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,5 +1,16 @@
 class Bank
+attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end 
+
+  def deposit(amount)
+    @balance += amount
+  end
+
   def printBalance
     'date || credit || debit || balance'
   end
+
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,4 +4,11 @@ describe Bank do
   it "Can print bank statement title" do
   expect {subject.printBalance().to output('date || credit || debit || balance')} 
   end 
+
+  context '#deposit' do
+  it "Can deposit an amount into the account" do 
+    bank = Bank.new
+    expect {bank.deposit(100)}.to change {bank.balance}.by(100)
+  end 
+end
 end
