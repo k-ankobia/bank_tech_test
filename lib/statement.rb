1ab90fe62e65f2013d1c2c89038b64ab4e48a712
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-03-11 17:58:15 +0000

    :sparkles: User can print current date

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,5 +1,11 @@
 class Statement
+  attr_reader :date
   def print_balance
     'date || credit || debit || balance'
+  end
+
+  def get_date
+    @date = Time.now.strftime('%Y-%m-%d')
+    @date
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,9 +1,18 @@
 require 'statement'
 
 describe Statement do
-  context '#printBalance' do
+  context '.printBalance' do
     it 'Can print bank statement title' do
       expect { subject.printBalance.to output('date || credit || debit || balance') }
+    end
+  end
+
+  context '#date' do
+    it 'Can print the current date' do
+      # @date = Time.now.strftime('%Y-%m-%d')
+      # allow(Time).to receive(:now).and_return(@date)
+      # expect { subject.get_date }.to output(@date).to_stdout
+      expect { subject.get_date.to output("2019-03-11")}
     end
   end
 end
